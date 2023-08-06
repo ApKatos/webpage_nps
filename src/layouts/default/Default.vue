1e823902ac98a72417c0adacfc1adcb6fd11f477
Patrícia Sotáková <patricia.sotakova@natur.cuni.cz>
2023-08-06 11:27:50 +0200

    Create footer and have to option to add it

--- src/layouts/default/Default.vue
+++ src/layouts/default/Default.vue
@@ -2,10 +2,12 @@
   <v-app>
     <default-bar />
     <default-view />
+    <!-- <footer-bar-view /> -->
   </v-app>
 </template>
 
 <script lang="ts" setup>
-  import DefaultBar from './AppBar.vue'
-  import DefaultView from './View.vue'
+import DefaultBar from './AppBar.vue'
+import DefaultView from './View.vue'
+import FooterBarView from './FooterBar.vue';
 </script>
---
+++ src/layouts/default/FooterBar.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-footer color="grey" density="compact" fixed>
+        <div>
+            <!-- <v-btn v-for="item in resources" :key="item.obj" class="mx-4" :icon="item.icon" variant="text"></v-btn> -->
+            <v-col>
+                {{ new Date().getFullYear() }} — <strong>Network phenotyping strategy</strong>
+            </v-col>
+        </div>
+    </v-footer>
+</template>
+<script>
+import { PxArticleMultiple } from "oh-vue-icons/icons";
+import { SiSpringCreators } from "oh-vue-icons/icons";
+import { RiComputerLine } from "oh-vue-icons/icons";
+export default {
+    components: { PxArticleMultiple },
+    data() {
+        return {
+            resources: [
+                { icon: PxArticleMultiple, obj: "article", value: "https://aacrjournals.org/cancerres/article/70/8_Supplement/2010/562508/Abstract-2010-A-Network-Phenotyping-Strategy-NPS" },
+                { icon: SiSpringCreators, obj: "algorithm", value: "Petr Pančoška, Jaroslav Nešetřil" },
+                { icon: RiComputerLine, obj: "webpage", value: "Patrícia Sotáková" },
+            ]
+        }
+    }
+}
+</script>
