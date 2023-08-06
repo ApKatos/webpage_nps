b2c2116f1aab840372ac9e361cf275dc08a75b5f
cechovic <markocechovic@gmail.com>
2023-08-06 14:25:58 +0200

    fix: info tag css

--- src/components/InfoTag.vue
+++ src/components/InfoTag.vue
@@ -1,48 +1,49 @@
 <template>
-    <div class="tag" @mouseover=" hover = true" @mouseleave="hover = false">
-        <v-icon class="tag-icon" color="black" icon="mdi-information" size="large">
-
-        </v-icon>
-     <div class="tag-text" v-if="hover">
-                    <!-- TODO: add the gaps at the beginning and end of each line -->
-                    <!-- &nbsp; &nbsp; -->
-                    <slot>
-                    </slot>
-                    <!-- &nbsp; &nbsp; -->
-                </div>
+  <div class="tag" @mouseover="hover = true" @mouseleave="hover = false">
+    <v-icon class="tag-icon" color="black" icon="mdi-information" size="large">
+    </v-icon>
+    <div class="tag-text" v-if="hover">
+      <!-- TODO: add the gaps at the beginning and end of each line -->
+      <!-- &nbsp; &nbsp; -->
+      <slot> </slot>
+      <!-- &nbsp; &nbsp; -->
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            hover: false
-        }
-    },
-}
+  data() {
+    return {
+      hover: false,
+    };
+  },
+};
 </script>
 
 <style scoped>
 .tag {
-    position: absolute;
-    /* z-index: 100; */
-    padding: 5px;
-    display: flex;
+  position: absolute;
+  /* z-index: 100; */
+  padding: 5px;
+  display: flex;
 }
 
 .tag-icon {
-    cursor: pointer;
+  cursor: pointer;
 }
 
 .tag-text {
-    position: relative;
-    /* width: 200px; */
-    background: whitesmoke;
-    /* border: dashed; */
-    /* border-width: 2px 4px; */
-    border-radius: 30px;
-    white-space: pre-wrap;
-    color: black;
+  position: relative;
+  /* width: 200px; */
+  background: whitesmoke;
+  /* border: dashed; */
+  /* border-width: 2px 4px; */
+  border-radius: 30px;
+  white-space: pre-wrap;
+  color: black;
+  padding: 40px;
+  min-width: 262px;
+  text-align: justify;
 }
 </style>
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,54 +1,83 @@
 <template>
   <div class="bg-success">
     <div class="varbox">
-      <infoTag style="margin-left: 14em; width: 14em;">{{ this.variablesinfo }}</infoTag>
+      <infoTag style="position: fixed; top: 85px; left: 15vw; margin-top: -38px;">{{
+        this.variablesinfo
+      }}</infoTag>
       <var-box :variables="variables" app density="compact"></var-box>
     </div>
     <div>
-      <div class="container" v-for="variable in variables " :key="variable.varname">
+      <div
+        class="container"
+        v-for="variable in variables"
+        :key="variable.varname"
+      >
         <div class="graph" :id="'input-graph-' + variable.index">
-
           <p>{{ variable.value }}</p>
           <infoTag>{{ variable.decription }}</infoTag>
-          <HistogramC2 :labelsInp="variable.labels" :dataInp="variable.data" :dataName="variable.varname"
-            @update:value="variable.value = $event" :value="variable.value">
+          <HistogramC2
+            :labelsInp="variable.labels"
+            :dataInp="variable.data"
+            :dataName="variable.varname"
+            @update:value="variable.value = $event"
+            :value="variable.value"
+          >
           </HistogramC2>
           <div v-if="variable.categ">
-            <BinaryButtons :labels="variable.labels" v-on:update:value="variable.value = $event" :value="variable.value"
-              :thrs="variable.thrs">
+            <BinaryButtons
+              :labels="variable.labels"
+              v-on:update:value="variable.value = $event"
+              :value="variable.value"
+              :thrs="variable.thrs"
+            >
             </BinaryButtons>
           </div>
           <div v-else>
-            <MySlider :label95q-lower="variable.alert_low" :label95q-upper="variable.alert_high" :maxim="variable.max"
-              :minim="variable.min" :relevant-thresholds="variable.thrs" :categorial="variable.categ"
-              :value="variable.value" v-on:update:value="variable.value = $event">
+            <MySlider
+              :label95q-lower="variable.alert_low"
+              :label95q-upper="variable.alert_high"
+              :maxim="variable.max"
+              :minim="variable.min"
+              :relevant-thresholds="variable.thrs"
+              :categorial="variable.categ"
+              :value="variable.value"
+              v-on:update:value="variable.value = $event"
+            >
             </MySlider>
           </div>
         </div>
       </div>
       <div class="endbutton">
-        <v-btn @click="callModel()" :disabled="!allVarsSubmitted" :loading="pressedButton">
+        <v-btn
+          @click="callModel()"
+          :disabled="!allVarsSubmitted"
+          :loading="pressedButton"
+        >
           SUBMIT
         </v-btn>
-        <infoTag v-if="allVarsSubmitted"> Check if all values in the left panel are correct and submit</infoTag>
-        <infoTag v-else> Fill all variables before submitting the input parameters</infoTag>
+        <infoTag v-if="allVarsSubmitted">
+          Check if all values in the left panel are correct and submit</infoTag
+        >
+        <infoTag v-else>
+          Fill all variables before submitting the input parameters</infoTag
+        >
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import HistogramC2 from "../components/Histogram_C2.vue"
-import jsonData from "../json/pats_fixedbug_intervals.json"
-import MySlider from "../components/Slider.vue"
-import BinaryButtons from "@/components/BinaryButtons.vue"
-import VarBox from '../components/varInfoBox.vue'
-import infoTag from '../components/InfoTag.vue'
-import { ModelWrapper } from '../utils/Model.ts';
+import HistogramC2 from "../components/Histogram_C2.vue";
+import jsonData from "../json/pats_fixedbug_intervals.json";
+import MySlider from "../components/Slider.vue";
+import BinaryButtons from "@/components/BinaryButtons.vue";
+import VarBox from "../components/varInfoBox.vue";
+import infoTag from "../components/InfoTag.vue";
+import { ModelWrapper } from "../utils/Model.ts";
 
 // prepisat  labels z 0,1 na NO, YES
 // upravit max range pri AFP (?) na nizsie cislo
-// pridat steps 1 k premennym AGE, TUMOR_NODULES 
+// pridat steps 1 k premennym AGE, TUMOR_NODULES
 export default {
   components: { HistogramC2, MySlider, BinaryButtons, VarBox, infoTag },
   data() {
@@ -57,7 +86,8 @@ export default {
       model: [],
       pressedButton: false,
       resultJSON: "",
-      variablesinfo: "Color of the parameter informs you about the prevalence of chosen value in population this model was trained on.\nRed - unfilled value\nOrange - Very scarcely observed value\nGreen - average value\nPressing the variable name will navigate you to input value for the field."
+      variablesinfo:
+        "Color of the parameter informs you about the prevalence of chosen value in population this model was trained on.\nRed - unfilled value\nOrange - Very scarcely observed value\nGreen - average value\nPressing the variable name will navigate you to input value for the field.",
     };
   },
   methods: {
@@ -70,45 +100,46 @@ export default {
     },
     createJSONfromVariables(message, event) {
       if (event && this.allVarsSubmitted) {
-
         let arr = {};
         for (let i = 0; i < this.variables.length; i++) {
-          let variable = this.variables[i]
-          arr[variable.varname] = variable.value
+          let variable = this.variables[i];
+          arr[variable.varname] = variable.value;
         }
-        console.log(arr)
-        let JSONModelInput = JSON.stringify(arr)
+        console.log(arr);
+        let JSONModelInput = JSON.stringify(arr);
         return JSONModelInput;
       } else {
-        alert(message)
+        alert(message);
       }
     },
     initModel() {
-      this.model = new ModelWrapper()
+      this.model = new ModelWrapper();
     },
     callModel() {
-      let inputJSON = this.createJSONfromVariables('Form cannot be submitted yet.', this.allVarsSubmitted);
-      let resultJSON = this.model.process(inputJSON)
+      let inputJSON = this.createJSONfromVariables(
+        "Form cannot be submitted yet.",
+        this.allVarsSubmitted
+      );
+      let resultJSON = this.model.process(inputJSON);
       this.resultJSON = resultJSON;
-      this.$router.push({ name: 'Result', query: { result: resultJSON } });
-
-    }
+      this.$router.push({ name: "Result", query: { result: resultJSON } });
+    },
   },
   mounted() {
     this.loadData();
-    this.initModel()
+    this.initModel();
   },
   computed: {
     allVarsSubmitted() {
       for (let variable of this.variables) {
         if (variable.value == null) {
-          return false
+          return false;
         }
       }
-      return true
+      return true;
     },
   },
-}
+};
 </script>
 
 <style scoped>
@@ -119,7 +150,7 @@ export default {
 
 /* .bg {
   background-image: linear-gradient(0.35turn, gray, lightgreen);
-  
+
 } */
 
 .container {
@@ -145,9 +176,7 @@ export default {
   opacity: 0.88;
   border: 3px solid green;
   background-color: white;
-  transition:
-    opacity 0s ease,
-    width 0s ease-in-out box-shadow 0.3s linear,
+  transition: opacity 0s ease, width 0s ease-in-out box-shadow 0.3s linear,
     background-color 0s ease;
 }
 
