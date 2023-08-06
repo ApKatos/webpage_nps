e5bde3e71a84349703cefa49558a5ed1ad51fd9f
Patrícia Sotáková <patricia.sotakova@natur.cuni.cz>
2023-08-06 11:30:21 +0200

    Create component to be added on a page and display informative text

---
+++ src/components/InfoTag.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="tag" @mouseover=" hover = true" @mouseleave="hover = false">
+        <v-icon class="tag-icon" color="black" icon="mdi-information" size="large">
+        </v-icon>
+        <div class="tag-text" v-if="hover">
+            <!-- TODO: add the gaps at the beginning and end of each line -->
+            <!-- &nbsp; &nbsp; -->
+            <slot>
+            </slot>
+            <!-- &nbsp; &nbsp; -->
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            hover: false
+        }
+    },
+}
+</script>
+
+<style scoped>
+.tag {
+    padding: 5px;
+    display: flex;
+}
+
+.tag-icon {
+    cursor: pointer;
+}
+
+.tag-text {
+    /* position: absolute; */
+    background: whitesmoke;
+    /* border: dashed; */
+    /* border-width: 2px 4px; */
+    border-radius: 30px;
+    white-space: pre-wrap;
+    color: black;
+}
+</style>
