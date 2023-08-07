de0eb30a12127f68d5209f90b1b8e982bd9cf522
Patrícia Sotáková <patricia.sotakova@natur.cuni.cz>
2023-08-07 19:05:32 +0200

    Fix padding for text display

--- src/components/WarnUser.vue
+++ src/components/WarnUser.vue
@@ -1,7 +1,7 @@
 <template>
     <v-sheet class="d-flex align-center justify-center flex-wrap text-center mx-auto" elevation="4" height="250" rounded
         max-width="800" width="100%">
-        <div>
+        <div style="padding: 1em;">
             <h2 class="text-h4 font-weight-black text-orange">ALERTING REMINDER</h2>
 
             <div class="text-h5 font-weight-medium mb-2" style="padding: 05px 50px 0px 50px">
