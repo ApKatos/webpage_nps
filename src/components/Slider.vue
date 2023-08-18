fca1cea3881eb26bb790478eee2d3027169ab929
Patrícia Sotáková <patricia.sotakova@natur.cuni.cz>
2023-08-18 15:42:23 +0200

    Fix responsive width of slider, and increasing/decreasing of values

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -1,18 +1,17 @@
 <template>
-  <v-card width=35vw style="padding: 20px 0px 0px 0px">
+  <v-card style="padding: 20px 0px 0px 0px; width: calc(100% - 18px);">
 
-    <v-slider v-model="valueData" step=0.1 density='compact' :color="color" thumb-size=10 thumb-label="always" hight=50
-      :max="maxim" :min="minim">
+    <v-slider v-model="valueData" step=0.1 density='compact' :color="color" thumb-size=10 thumb-label="always" hight=70
+      :min="minim" :max="maxim">
 
       <template v-slot:prepend>
-        <v-btn size="small" variant="text" icon="mdi-minus" color="red" @click="decrement(1)"
-          @mousedown="startDecrement()" @mouseup="stopDecrement" @mouseleave="stopDecrement"></v-btn>
+        <v-btn size="small" variant="text" icon="mdi-minus" color="red" @click.once="decrement(1)"
+          @mousedown="startDecrement" @mouseup="stopDecrement" @mouseleave="stopDecrement"></v-btn>
       </template>
 
-      <!-- BUTTON 2 -->
       <template v-slot:append>
-        <v-btn size="small" variant="text" icon="mdi-plus" color="green" @click="increment(1)"
-          @mousedown="startIncrement()" @mouseup="stopIncrement" @mouseleave="stopIncrement"></v-btn>
+        <v-btn size="small" variant="text" icon="mdi-plus" color="green" @click.once="increment(1)"
+          @mousedown="startIncrement" @mouseup="stopIncrement" @mouseleave="stopIncrement"></v-btn>
       </template>
 
     </v-slider>
@@ -25,7 +24,6 @@ export default {
   data() {
     return {
       valueData: this.value,
-      // clickTimeout: null, //https://blog.logrocket.com/building-a-long-press-directive-in-vue-3408d60fb511/
       color: "#a9a9a9",
       incrementInterval: null, // Variable to hold the interval ID
       decrementInterval: null // Variable to hold the interval ID
@@ -60,10 +58,9 @@ export default {
       this.valueData = Math.max(0, Number((this.valueData - (0.1 * times)).toFixed(1)));
     },
     startIncrement() {
-      console.log("idem incrementovat " + typeof this.valueData)
       this.incrementInterval = setInterval(() => {
         this.increment(10); // Increment the variable value
-      }, 50); // Adjust the interval duration as per your preference
+      }, 50);
     },
     stopIncrement() {
       clearInterval(this.incrementInterval); // Stop the increment when the button is released or the cursor leaves the button area
@@ -72,7 +69,7 @@ export default {
     startDecrement() {
       this.decrementInterval = setInterval(() => {
         this.decrement(10); // Increment the variable value
-      }, 50); // Adjust the interval duration as per your preference
+      }, 50);
     },
     stopDecrement() {
       clearInterval(this.decrementInterval); // Stop the increment when the button is released or the cursor leaves the button area
@@ -80,14 +77,6 @@ export default {
     },
   },
   computed: {
-    // valueData: {
-    //   get() {
-    //     return this.value; // Set the selected value based on the prop value
-    //   },
-    //   set(newValue) {
-    //     this.$emit('update:value', newValue); // Emit the updated value to the parent component
-    //   },
-    // },
     colorWarn: {
       get() {
         return "#D3A350"
@@ -105,7 +94,6 @@ export default {
       handler(val) {
         this.$emit('update:value', val)
 
-        // ASI TO JE POMALE KED JE TO TU STALE
         if (val != null && (val <= this.label95qLower || val >= this.label95qUpper)) {
           this.color = this.colorWarn
         } else if (val != null && val < this.label95qUpper && val > this.label95qLower) {
