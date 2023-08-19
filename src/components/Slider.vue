4fea0644b35114dffb34f7a64ef27fe8f159e30f
Patrícia Sotáková <patricia.sotakova@natur.cuni.cz>
2023-08-19 10:41:23 +0200

    Fix adding and subtracting value with Slider on devices based on touch

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -6,12 +6,14 @@
 
       <template v-slot:prepend>
         <v-btn size="small" variant="text" icon="mdi-minus" color="red" @click.once="decrement(1)"
-          @mousedown="startDecrement" @mouseup="stopDecrement" @mouseleave="stopDecrement"></v-btn>
+          @mousedown="startDecrement" @mouseup="stopDecrement" @mouseleave="stopDecrement" @touchstart="startDecrement"
+          @touchend="stopDecrement" @touchmove="stopDecrement" @touchcancel="stopDecrement"></v-btn>
       </template>
 
       <template v-slot:append>
         <v-btn size="small" variant="text" icon="mdi-plus" color="green" @click.once="increment(1)"
-          @mousedown="startIncrement" @mouseup="stopIncrement" @mouseleave="stopIncrement"></v-btn>
+          @mousedown="startIncrement" @mouseup="stopIncrement" @mouseleave="stopIncrement" @touchstart="startIncrement"
+          @touchend="stopIncrement" @touchup="stopIncrement" @touchcancel="stopIncrement"></v-btn>
       </template>
 
     </v-slider>
@@ -58,18 +60,23 @@ export default {
       this.valueData = Math.max(0, Number((this.valueData - (0.1 * times)).toFixed(1)));
     },
     startIncrement() {
-      this.incrementInterval = setInterval(() => {
-        this.increment(10); // Increment the variable value
-      }, 50);
+      if (this.incrementInterval === null) {
+        this.incrementInterval = setInterval(() => {
+          this.increment(10); // Increment the variable value
+        }, 50);
+      }
+
     },
     stopIncrement() {
       clearInterval(this.incrementInterval); // Stop the increment when the button is released or the cursor leaves the button area
       this.incrementInterval = null;
     },
     startDecrement() {
-      this.decrementInterval = setInterval(() => {
-        this.decrement(10); // Increment the variable value
-      }, 50);
+      if (this.decrementInterval === null) {
+        this.decrementInterval = setInterval(() => {
+          this.decrement(10); // Increment the variable value
+        }, 50);
+      }
     },
     stopDecrement() {
       clearInterval(this.decrementInterval); // Stop the increment when the button is released or the cursor leaves the button area
@@ -93,7 +100,6 @@ export default {
     valueData: {
       handler(val) {
         this.$emit('update:value', val)
-
         if (val != null && (val <= this.label95qLower || val >= this.label95qUpper)) {
           this.color = this.colorWarn
         } else if (val != null && val < this.label95qUpper && val > this.label95qLower) {
