c8500885e5bd5282667465350f26540782dce37e
Patrícia Sotáková <patricia.sotakova@natur.cuni.cz>
2023-08-23 14:02:14 +0200

    Adjust the webpage to relative routing to base

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,17 +7,17 @@ const routes = [
     component: () => import("@/layouts/default/Default.vue"),
     children: [
       {
-        path: "/",
+        path: "", // No base needed for the root path
         name: "Home",
         component: () => import("@/views/Home.vue"),
       },
       {
-        path: "main",
+        path: "main", // No base needed for this path
         name: "Main",
         component: () => import("@/views/Main.vue"),
       },
       {
-        path: "result",
+        path: "result", // No base needed for this path
         name: "Result",
         component: () => import("@/views/Results.vue"),
         props: (route: any) => ({ result: route.query.result }),
@@ -26,8 +26,16 @@ const routes = [
   },
 ];
 
+const base = import.meta.env.BASE_URL.endsWith("/")
+  ? import.meta.env.BASE_URL
+  : import.meta.env.BASE_URL + "/";
+
+routes.forEach((route) => {
+  route.path = base + route.path;
+});
+
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHistory(),
   routes,
 });
 
